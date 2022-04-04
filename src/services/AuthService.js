2eb5a91762c6a97c7947c0887be1282f1c540594
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-04 11:21:51 +0500

    added history, AuthService

---
+++ src/services/AuthService.js
@@ -0,0 +1,51 @@
+const apiUrl = 'http://localhost:3000/';
+
+const handleResponse = (response) => {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                logout();
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
+
+const login = async (username, password) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    const response = await fetch(`${apiUrl}users/login`, requestOptions);
+    const user = await handleResponse(response);
+    localStorage.setItem('user', JSON.stringify(user));
+    return user;
+}
+
+const logout = () => {
+    localStorage.removeItem('user');
+}
+
+const register = async (user) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    const response = await fetch(`${apiUrl}users/register`, requestOptions);
+    return handleResponse(response);
+}
+
+export const authService = {
+    login,
+    logout,
+    register,
+};
