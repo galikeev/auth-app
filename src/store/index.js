b1de510df3b4c0978ed6660230ca89e771e441c4
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-04 11:22:25 +0500

    added thunkMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
-import { createStore } from "redux";
-import reducer from '../reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from '../reducers';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+);
 
 export default store;
