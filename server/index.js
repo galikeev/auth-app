b715786a5d9cd048cf51b50d00df9a271c9e8bcc
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-05 22:09:27 +0500

    started server and hide folders

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log('hello world')
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const authRouter = require('./routes/auth.routes');
+const app = express();
+const PORT = config.get('serverPort');
+
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
+const start = async () => {
+    try {
+        mongoose.connect(config.get('dbUrl')); 
+
+        app.listen(PORT, () => {
+            console.log('server started on port', PORT);
+        });
+    } catch(e) {
+
+    }
+}
+
+start();
