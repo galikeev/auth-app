0d88de7dcef1ef16ee668322a2a2963edeed916b
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-05 22:05:42 +0500

    auth routes

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,72 @@
+const Router = require('express');
+const User = require('../models/User'); // модель пользователя
+const bcrypt = require('bcryptjs'); // пакет для хеширования пароля
+const config = require('config');
+const jwt = require('jsonwebtoken'); 
+const {check, validationResult} = require('express-validator');
+const router = new Router();
+
+router.post('/registration', 
+    [
+        check('email', 'Incorrect email').isEmail(),
+        check('password', 'Password must be longer than 3 and shorter than 12').isLength({min: 3, max: 12})
+    ],
+    async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({message: 'Incorrect request'})
+        }
+        const {email, password, firstName, lastName, userName} = req.body;
+
+        const candicate = await User.findOne({email});
+
+        if (candicate) {
+            return res.status(400).json({message: `User with email ${email} already exist`});
+        }
+        const hashPassword = await bcrypt.hash(password, 8);
+        const user = new User({email, password: hashPassword, firstName, lastName, userName});
+
+        await user.save();
+
+        return res.json({message: "User was created"});
+
+    } catch (e) {
+        console.log(e);
+        res.send({message: 'Server error'})
+    }
+});
+
+router.post('/login', 
+    async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if (!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        const isPassValid = bcrypt.compareSync(password, user.password);
+        if (!isPassValid) {
+            return res.status(400).json({message: 'Invalid password'})
+        }
+
+        const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'});
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                userName: user.userName
+            }
+        })
+
+    } catch (e) {
+        console.log(e);
+        res.send({message: 'Server error'})
+    }
+});
+
+module.exports = router;
