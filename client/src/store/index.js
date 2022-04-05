85a45fb37c5418decdd5a0031c7c4a133b54d812
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-05 22:42:47 +0500

    fixed store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
