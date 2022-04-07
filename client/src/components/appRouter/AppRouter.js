ecf4c169b0727e7bc1cde33403d9486a56562654
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-07 16:36:50 +0500

    rendering of elements depending on conditions

--- client/src/components/appRouter/AppRouter.js
+++ client/src/components/appRouter/AppRouter.js
@@ -3,42 +3,34 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { authRoutes, publicRoutes } from '../../routes';
-import { history } from '../../services/history';
 import { LOGIN_ROUTE } from '../../utils/routesConsts';
-import {alertActions} from '../../actions';
-
-const connectedApp = (props) => {
-
-    history.listen((location, action) => {
-        // clear alert on location change
-        props.clearAlerts();
-    });
-
-    return (
-        <div className='container'>
-            {props.alert.message && <div className={props.alert.type}>{props.alert.message}</div>}
-            <Routes history={history}>
-                {localStorage.getItem('user') && authRoutes.map(({path, Component}) => 
-                    <Route key={path} path={path} element={<Component/>}/>
-                )}
-                {publicRoutes.map(({path, Component}) => 
-                    <Route key={path} path={path} element={<Component/>}/>
-                )}
-                <Route path='*' element={<Navigate to={LOGIN_ROUTE} replace/>}/>
-            </Routes>
-        </div>
-    );
-};
 
 function mapState(state) {
-    const { alert } = state;
-    return { alert };
+    return {
+        state: state
+    }
 }
 
-const actionCreators = {
-    clearAlerts: alertActions.clear
-};
+const AppRouter = connect(mapState)(({state}) => {
 
-const AppRouter = connect(mapState, actionCreators)(connectedApp);
+    const isLogged = state.signIn.isLogged;
+
+    return (
+        <>
+            {state.alert.message && <div className={state.alert.type} style={{'textAlign':'center'}}>{state.alert.message}</div>}
+            <div className='container'>
+                {!isLogged && <Routes>
+                    {localStorage.getItem('user') && authRoutes.map(({path, Component}) => 
+                        <Route key={path} path={path} element={<Component/>}/>
+                    )}
+                    {publicRoutes.map(({path, Component}) => 
+                        <Route key={path} path={path} element={<Component/>}/>
+                    )}
+                    <Route path='*' element={<Navigate to={LOGIN_ROUTE} replace/>}/>
+                </Routes>}
+            </div>
+        </>
+    );
+});
 
 export default AppRouter;
--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -1,8 +1,17 @@
-import Logo from '../../assets/img/reactjs-icon.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
+import { userActions } from '../../actions';
+
+import Logo from '../../assets/img/reactjs-icon.svg';
 import './navBar.scss';
 
 const NavBar = () => {
+
+    const dispatch = useDispatch();
+
+    const isLogged = useSelector(state => state.signIn.isLogged);
+
     return (
         <div className="navbar">
             <div className="navbar__wrapper">
@@ -10,8 +19,9 @@ const NavBar = () => {
                 <a href="1" className="navbar__header">AUTH APP</a>
             </div>
             <div className="navbar__wrapper">
-                <div className="navbar__login">Войти</div>
-                <div className="navbar__registration">Регистрация</div>
+                {!isLogged && <div className="navbar__login"><NavLink to="/login">Войти</NavLink></div>}
+                {!isLogged && <div className="navbar__registration"><NavLink to="/registration">Регистрация</NavLink></div>}
+                {isLogged && <button className="navbar__logout btn btn-primary btn-block" onClick={() => dispatch(userActions.logout())}>Выйти</button>}
             </div>
         </div>
     );
