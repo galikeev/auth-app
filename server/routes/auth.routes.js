487c5305097263730416fa55dc02f87c544871db
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-08 10:12:18 +0500

    function for get user data

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,7 @@ const config = require('config');
 const jwt = require('jsonwebtoken'); 
 const {check, validationResult} = require('express-validator');
 const router = new Router();
+const authMiddleware = require('../middleware/auth.middleware');
 
 router.post('/registration', 
     [
@@ -61,8 +62,29 @@ router.post('/login',
                 lastName: user.lastName,
                 userName: user.userName
             }
-        })
+        });
 
+    } catch (e) {
+        console.log(e);
+        res.send({message: 'Server error'})
+    }
+});
+
+router.post('/auth', authMiddleware, 
+    async (req, res) => {
+    try {
+        const user = await User.findOne({id: req.user.id});
+        const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '1h'});
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                userName: user.userName
+            }
+        });
     } catch (e) {
         console.log(e);
         res.send({message: 'Server error'})
