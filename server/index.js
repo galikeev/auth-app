9d11ce5591410b765458148266bcf51191dc3311
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-13 13:17:08 +0500

    removed static

--- server/index.js
+++ server/index.js
@@ -11,22 +11,11 @@ app.use(corsMiddleware);
 app.use(express.json());
 app.use('/api/auth', authRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('../client/build'));
-
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
-
 const start = async () => {
     try {
         mongoose.connect(URL); 
 
-        app.listen(PORT, "0.0.0.0", () => {
+        app.listen(PORT, () => {
             console.log('server started on port', PORT);
         });
     } catch(e) {
