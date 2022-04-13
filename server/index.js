cf29c8fb3f35bee262fc048dc728754ff834d50c
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-13 14:00:34 +0500

    heroku

--- server/index.js
+++ server/index.js
@@ -6,10 +6,21 @@ const app = express();
 const PORT = process.env.PORT || config.get('serverPort');
 const URL = process.env.dbUrl || config.get('dbUrl');
 const corsMiddleware = require('./middleware/cors.middleware');
+const path = require('path');
 
 app.use(corsMiddleware);
 app.use(express.json());
 app.use('/api/auth', authRouter);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Enable push-state routing (e.g. with React Router's BrowserRouter)
+app.get('*', (req, res, next) => {
+    if (req.url.startsWith('/api/auth')) {
+        return next();
+    }
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 const start = async () => {
     try {
