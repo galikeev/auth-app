93dae9c6e5a3ca3e7b998011e2b9ff10f0382fe8
Ramil Galikeev <galikeevramil95@gmail.com>
2022-05-14 19:10:58 +0500

    added username on homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
+
 
 const HomePage = () => {
+
+	const user = useSelector(state => state.signIn.user);
 
 		return (
 			<div className="text-center" style={{'marginTop': '50px'}}>
 				<h2>Login Successful!</h2>
-				<h3>Welcome to Home Page</h3>
+				<h3>Hello {user.user.userName}</h3>
 			</div>
 		)
 };
